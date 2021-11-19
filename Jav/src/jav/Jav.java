6771f8d65833b3ea8dcdcd422f6c55cb34d0c5f4
MohonaJS <89448559+MohonaJS@users.noreply.github.com>
2021-11-19 21:12:52 +0100

    Add files via upload

---
+++ Jav/src/jav/Jav.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jav;
+
+/**
+ *
+ * @author chowd
+ */
+public class Jav {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     int a=50, b=20, c=5;
+     if((a==50 && b==10)|| c==5){
+         System.out.println("sant");
+     
+     }else{
+         System.out.println("falskt");
+     
+     }
+        
+    }
+    
+}
